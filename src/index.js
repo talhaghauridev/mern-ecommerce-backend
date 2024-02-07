88dc363fdcca16ab8f3bbe5e8b944e4f5f7d63c5
Talha Ghouri <talhaghouridev@gmail.com>
2024-02-06 16:12:15 -0800

    remove unuse things

--- src/index.js
+++ src/index.js
@@ -2,50 +2,13 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
 const connectDB = require("./db/index");
-const Stripe = require("stripe");
-const bodyParser = require('body-parser');
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 //Confing
 
 dotenv.config({ path: "./.env" });
 app.use(bodyParser.json());
 // Connecting to Database
-
-app.post(
-  "/webhook",
-  bodyParser.raw({ type: "application/json" }),
-  (req, res) => {
-    const sig = req.headers["stripe-signature"];
-
-    let event;
-
-    try {
-      event = stripe.webhooks.constructEvent(
-        req.body,
-        sig,
-        "whsec_f1310f308ed52e56f3498b0b0eb852da2ced78abf3674219ad104432d1532614"
-      );
-    } catch (err) {
-      console.error("Webhook Error:", err.message);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    // Handle the event
-    switch (event.type) {
-      case "payment_intent.succeeded":
-        const paymentIntent = event.data.object;
-        // Handle successful payment
-        break;
-      // Add more cases for other events as needed
-      default:
-        console.log(`Unhandled event type: ${event.type}`);
-    }
-
-    // Return a 200 response to acknowledge receipt of the event
-    res.json({ received: true });
-  }
-);
 
 let server;
 connectDB().then(() => {
